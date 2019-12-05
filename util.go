0927d18b8fe88ed8027e84f5c57d5b616661e187
Rifki <rifky_@outlook.com>
2019-12-05 13:52:35 +0700

    add function for check eligibility of kube event

--- util.go
+++ util.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 
-	core "k8s.io/api/core/v1"
+	log "github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
 	extensions "k8s.io/api/extensions/v1beta1"
 )
 
@@ -22,7 +24,7 @@ func findB(f []extensions.IngressBackend, s string) bool {
 	return false
 }
 
-func findN(n []core.Namespace, s string) (string, bool) {
+func findN(n []v1.Namespace, s string) (string, bool) {
 	_ = len(n)
 	for _, v := range n {
 		lb := v.GetLabels()
@@ -41,6 +43,38 @@ func findI(i []extensions.Ingress, name string, namespace string) bool {
 		if v.Name == name && v.Namespace == namespace {
 			return true
 		}
+	}
+
+	return false
+}
+
+func isEligibleForSync(obj interface{}) bool {
+	switch obj.(type) {
+	case *v1.Namespace:
+		nms := obj.(*v1.Namespace)
+		lb := nms.Labels
+		log.Info("Check if Namespace ",nms.Name,", event is elligible for update")
+
+		_, found := lb["dynamic-ingress/auto"]
+		_, found2 := findN(cacheNamespaces, nms.Name)
+		if found || found2 {
+			return true
+		}
+	case *v1.Service:
+		svc := obj.(*v1.Service)
+		lb := svc.Labels
+		log.Info("Check if Service ",svc.Name,", event is elligible for update")
+
+		val, found := lb["dynamic-ingress/auto"]
+		_, found2 := findN(cacheNamespaces, svc.Namespace)
+		if found2 {
+			return true
+		}else if (found && val == "enabled"){
+			return true
+		}
+	default:
+		log.Info(reflect.TypeOf(obj))
+		return false
 	}
 
 	return false
